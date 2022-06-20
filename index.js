59963b538d333d2a6510380c380b2c5b0ee7dfb0
cham0287 <62415003+cham0287@users.noreply.github.com>
2022-06-20 14:38:54 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const express = require('express');
 const app = express();
 const port = 7070;
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    'mongodb+srv://admin:7216@boilerplate.nmhbm.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(config.mongoURI)
   .then(() => console.log('mongoDB connected'))
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보를 client에서 받아서 데이터베이스에 넣어주기
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
 app.listen(port, () => console.log(`listening on ${port}`));
